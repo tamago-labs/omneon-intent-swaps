7ce9bec7f876d30477fa12986053e58d5ddb1db6
pisuthd <pisuth.dae@gmail.com>
2025-08-08 15:44:21 +0900

    Try fix pino-pretty error

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    webpack: (config) => {
+        config.externals.push('pino-pretty', /* add any other modules that might be causing the error */);
+        return config;
+    },
+}
 
 module.exports = nextConfig
